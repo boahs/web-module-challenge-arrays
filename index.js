141e3fb699fc4f61fb9f17f0bba14f291e0148a5
boahs <39522778+boahs@users.noreply.github.com>
2020-07-15 18:11:33 -0400

    task 7

--- index.js
+++ index.js
@@ -129,9 +129,11 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(newArr, arr) {
-  newArr = [...arr];
+function copy(arr) {
+  let newArr = [...arr];
+  return newArr;
 }
+let doggy = copy(kitty); // we're used pre-defined variables to assigned a copy. example : let doggy = ['bark'] let kitty = ['meow'] doggy will now be 'meow'
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -139,6 +141,7 @@ Your function should accept:
 
 (1) an Array
 (2) a string (i.e. "chocolate")
+
 
 and return a new array. 
 
@@ -148,8 +151,15 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/) {
-  /*code here*/
+function filterByWord(arr, str) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].includes(str)) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 }
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/
